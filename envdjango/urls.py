43a335672b60d634802967713108b8c9b05860f6
David <akindeindedavid@gmail.com>
2025-06-29 05:29:18 +0100

    Add the path object to url.py file for inference route

--- envdjango/urls.py
+++ envdjango/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from product.routes.test import test
+from product.routes.ask_gilbert import ask_gilbert
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),  
-    path('test/',test.as_view(),name="test")
+    path('test/',test.as_view(),name="test"),
+    path('ask-gilbert/',ask_gilbert.as_view(),name="ask-gilbert")
+
 ]
